e5346e85855876a380db5370933ea760f8e85ecd
goyalion17 <goyalion17@gmail.com>
2019-12-17 21:19:25 +0200

    create order, orders, modal

--- script/script.js
+++ script/script.js
@@ -7,9 +7,52 @@ document.addEventListener('DOMContentLoaded', () => {
     blockFreelancer = document.getElementById('block-freelancer'),
     blockChoice = document.getElementById('block-choice'),
     btnExit = document.getElementById('btn-exit'),
-    formCustomer = document.getElementById('form-customer');
+    formCustomer = document.getElementById('form-customer'),
+    ordersTable = document.getElementById('orders'),
+    modalOrder = document.getElementById('order_read'),
+    modalOrderActive = document.getElementById('order_active');
 
     const orders = [];
+
+    const renderOrders = () => {
+        ordersTable.textContent = '';
+
+        orders.forEach((order, i) => {
+            ordersTable.innerHTML += `
+            <tr class="order" data-number-order='${i}'>
+                <td>${i+1}</td>
+                <td>${order.title}</td>
+                <td class='${order.currency}'></td>
+                <td>${order.deadline}</td>
+            </tr>`
+        });
+    }
+
+    const openModal = (numberOrder) => {
+        const order = orders[numberOrder];
+        const modal = order.active ? modalOrderActive : modalOrder;
+
+        const firstNameBlock = document.querySelector('.firstName'),
+        titleBlock = document.querySelector('.modal-title'),
+        emailBlock = document.querySelector('.email'),
+        descriptionBlock = document.querySelector('.description'),
+        deadlineBlock = document.querySelector('.deadline'),
+        currencyBlock = document.querySelector('.currency_img'),
+        countBlock = document.querySelector('.count'),
+        phoneBlock = document.querySelector('.phone');
+
+        titleBlock.textContent = order.title;
+
+        modal.style.display = 'block';
+    };
+    
+    ordersTable.addEventListener('click', (event) => {
+        const target = event.target;
+        const targetOrder = target.closest('.order');
+        if(targetOrder) {
+            openModal(targetOrder.dataset.numberOrder);
+        }
+    })
 
     customer.addEventListener('click', () => {
         blockCustomer.style.display = 'block';
@@ -18,8 +61,9 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     
     freelancer.addEventListener('click', () => {
-        blockFreelancer.style.display = 'block';
         blockChoice.style.display = 'none';
+        renderOrders();
+        blockFreelancer.style.display = 'block';
         btnExit.style.display = 'block';
     });
     
@@ -33,8 +77,27 @@ document.addEventListener('DOMContentLoaded', () => {
     formCustomer.addEventListener('submit', (event) => {
         event.preventDefault();
         const obj = {};
+
+        const elements = [...formCustomer.elements].filter((elem) => ((elem.tagName === 'INPUT' && elem.type != 'radio') || (elem.type === 'radio' && elem.checked) || elem.tagName === 'TEXTAREA'));
+        elements.forEach((elem) => {
+            obj[elem.name] = elem.value;
+
+            if (elem.type !== 'radio') {
+                elem.value = '';
+            }
+        })
+
+        /* [...formCustomer.elements].forEach((elem) => {
+            if ((elem.tagName === 'INPUT' && elem.type != 'radio') || (elem.type === 'radio' && elem.checked) || elem.tagName === 'TEXTAREA') {
+                obj[elem.name] = elem.value;
+
+                if (elem.type !== 'radio') {
+                    elem.value = '';
+                }
+            } 
+        }); */
         
-        for (const elem of formCustomer.elements) {
+        /* for (const elem of formCustomer.elements) {
             if ((elem.tagName === 'INPUT' && elem.type != 'radio') || (elem.type === 'radio' && elem.checked) || elem.tagName === 'TEXTAREA') {
                 obj[elem.name] = elem.value;
 
@@ -42,7 +105,9 @@ document.addEventListener('DOMContentLoaded', () => {
                     elem.value = '';
                 }
             }  
-        }
+        } */
+
+        //formCustomer.reset();
 
         orders.push(obj);
     });
